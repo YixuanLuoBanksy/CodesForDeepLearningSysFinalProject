2a033b7fe7d28d6d20b5a7d5c1d57d1b1dcd0302
YixuanLuoBanksy <l.yixuan@wustl.edu>
2024-12-11 17:31:54 +0000

    test

---
+++ Codes/untitled.py
@@ -0,0 +1,51 @@
+from torch.profiler import profile, record_function, ProfilerActivity
+
+import os
+import time
+import math
+import pickle
+from contextlib import nullcontext
+
+import numpy as np
+import torch
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+from ours_models import GPTConfig, GPT_init, GPT_block
+
+block_num = 4
+n_layer = 12
+n_head = 12
+n_embd = 768
+block_size = 1024
+dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
+bias = False # do we use bias inside LayerNorm and Linear layers?
+model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
+                  bias=bias, vocab_size=None, dropout=dropout) # start with model_args from command line
+model_block1 = GPT_init(gptconf, block_num)
+inputs = torch.randn(64, 256)
+
+with profile(activities=[ProfilerActivity.CPU],profile_memory=True, record_shapes=True) as prof:
+    model_block1(inputs)
+
+print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
+
+# (omitting some columns)
+# ---------------------------------  ------------  ------------  ------------
+#                              Name       CPU Mem  Self CPU Mem    # of Calls
+# ---------------------------------  ------------  ------------  ------------
+#                       aten::empty      94.79 Mb      94.79 Mb           121
+#     aten::max_pool2d_with_indices      11.48 Mb      11.48 Mb             1
+#                       aten::addmm      19.53 Kb      19.53 Kb             1
+#               aten::empty_strided         572 b         572 b            25
+#                     aten::resize_         240 b         240 b             6
+#                         aten::abs         480 b         240 b             4
+#                         aten::add         160 b         160 b            20
+#               aten::masked_select         120 b         112 b             1
+#                          aten::ne         122 b          53 b             6
+#                          aten::eq          60 b          30 b             2
+# ---------------------------------  ------------  ------------  ------------
+# Self CPU time total: 53.064ms
+
+print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+
